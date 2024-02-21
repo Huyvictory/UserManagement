587fcba7b4bcedfd672211cda666d79c6cca17d5
Huyvictory <Huyvictory@HUYVICTORY-PC>
2024-02-22 00:51:31 +0700

    Add project files.

---
+++ UserManagement/Migrations/20240221165623_Added Custom Properties for table users.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserManagement.Migrations
+{
+    public partial class AddedCustomPropertiesfortableusers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ProfilePicture",
+                schema: "Identity",
+                table: "User",
+                type: "varbinary(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserNameChangeLimit",
+                schema: "Identity",
+                table: "User",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                schema: "Identity",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                schema: "Identity",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "ProfilePicture",
+                schema: "Identity",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "UserNameChangeLimit",
+                schema: "Identity",
+                table: "User");
+        }
+    }
+}
---
+++ UserManagement/Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UserManagement.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser(string firstName, string lastName, string userName, string email) : base()
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            UserName = userName;
+            Email = email;
+
+        }
+        public string FirstName { get; set; } = string.Empty;
+        public override string Email { get => base.Email; set => base.Email = value; }
+        public string LastName { get; set; } = string.Empty;
+        public int UserNameChangeLimit { get; set; } = 5;
+        public byte[]? ProfilePicture { get; set; }
+    }
+}
